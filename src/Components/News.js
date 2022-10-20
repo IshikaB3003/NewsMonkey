feeab802457ef041ae956f7d69262d9a55867069
IshikaB3003 <ishikabansal3003@gmail.com>
2022-10-20 19:21:19 +0530

    news cards setup

---
+++ src/Components/News.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import NewsItem from './NewsItem'
+
+export default class News extends Component {
+  render() {
+    return (
+      <div className='container my-3'>
+        <h2>NewMonkey - Top Headlines</h2>
+        <div className="row">
+            <div className="col-md-4">
+                <NewsItem title="Mytitle" description="mydesc"/>
+            </div>
+            <div className="col-md-4">
+                <NewsItem title="Mytitle" description="mydesc"/>
+            </div>
+            <div className="col-md-4">
+                    <NewsItem title="Mytitle" description="mydesc"/>
+                </div>
+            </div>
+      </div>
+    )
+  }
+}
+
+
