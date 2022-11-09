ed44c9d4586736ddbe2e5264e70005b060e9ba40
IshikaB3003 <ishikabansal3003@gmail.com>
2022-11-10 00:41:30 +0530

    news populated by fetching api

--- src/Components/News.js
+++ src/Components/News.js
@@ -4,67 +4,78 @@ import NewsItem from './NewsItem'
 
 export default class News extends Component {
 
-articles=[
-    {
-        "source": { "id": "bleacher-report", "name": "Bleacher Report" },
-        "author": null,
-        "title": "☄️ EMBIID-TATUM ANIME FIGHT☄️",
-        "description": "Fan easier, fan faster and fan better with Bleacher Report. Keep up with the latest storylines, expert analysis, highlights and scores for all your favorite sports.",
-        "url": "https://bleacherreport.com/videos/259156-joel-embiid-vs-jayson-tatum-epic-battle-hero-ball-episode-1",
-        "urlToImage": null,
-        "publishedAt": "2022-10-18T11:22:19.7782297Z",
-        "content": "EMBIID VS. TATUM IN EPIC FIGHT The battle for the face of the league begins. Hero Ball premieres NOW"
-      },
-      {
-        "source": { "id": "bleacher-report", "name": "Bleacher Report" },
-        "author": null,
-        "title": " Stefon Diggs Interview ",
-        "description": "Fan easier, fan faster and fan better with Bleacher Report. Keep up with the latest storylines, expert analysis, highlights and scores for all your favorite sports.",
-        "url": "https://bleacherreport.com/videos/255535-von-miller-x-stefon-diggs",
-        "urlToImage": null,
-        "publishedAt": "2022-10-13T16:37:36.6975172Z",
-        "content": null
-      },
-      {
-        "source": {
-          "id": "the-washington-times",
-          "name": "The Washington Times"
-        },
-        "author": "The Washington Times https://www.washingtontimes.com",
-        "title": "Latest Quizzes",
-        "description": "Take a break from the hard news of the day and enjoy a quiz on entertainment, sports, history and politics only from The Washington Times.",
-        "url": "https://www.washingtontimes.com/quiz/",
-        "urlToImage": null,
-        "publishedAt": "2022-08-30T16:37:43.8583104Z",
-        "content": "Featured Quizzes\r\nTake the challenge to learn about the life and career highlights of famed nonagenarian actress and comedian Betty White.\r\n Shares \r\nRead our synopsis and correctly identify a litera… [+32510 chars]"
-      },
-      {
-        "source": { "id": "usa-today", "name": "USA Today" },
-        "author": null,
-        "title": "Daily Briefing",
-        "description": "The day's top stories, from sports to movies to politics to world events.",
-        "url": "https://profile.usatoday.com/newsletters/daily-briefing/",
-        "urlToImage": "https://profile.usatoday.com/newsletters/resources/usat/property/usatoday/newsletter-thumbs/8872UT-E-NLETTER02@2x.jpg",
-        "publishedAt": "2021-08-15T15:35:07+00:00",
-        "content": "The day's top stories, from sports to movies to politics to world events."
-      }
-    ]
+// articles=[
+//     {
+//         "source": { "id": "bleacher-report", "name": "Bleacher Report" },
+//         "author": null,
+//         "title": "☄️ EMBIID-TATUM ANIME FIGHT☄️",
+//         "description": "Fan easier, fan faster and fan better with Bleacher Report. Keep up with the latest storylines, expert analysis, highlights and scores for all your favorite sports.",
+//         "url": "https://bleacherreport.com/videos/259156-joel-embiid-vs-jayson-tatum-epic-battle-hero-ball-episode-1",
+//         "urlToImage": null,
+//         "publishedAt": "2022-10-18T11:22:19.7782297Z",
+//         "content": "EMBIID VS. TATUM IN EPIC FIGHT The battle for the face of the league begins. Hero Ball premieres NOW"
+//       },
+//       {
+//         "source": { "id": "bleacher-report", "name": "Bleacher Report" },
+//         "author": null,
+//         "title": " Stefon Diggs Interview ",
+//         "description": "Fan easier, fan faster and fan better with Bleacher Report. Keep up with the latest storylines, expert analysis, highlights and scores for all your favorite sports.",
+//         "url": "https://bleacherreport.com/videos/255535-von-miller-x-stefon-diggs",
+//         "urlToImage": null,
+//         "publishedAt": "2022-10-13T16:37:36.6975172Z",
+//         "content": null
+//       },
+//       {
+//         "source": {
+//           "id": "the-washington-times",
+//           "name": "The Washington Times"
+//         },
+//         "author": "The Washington Times https://www.washingtontimes.com",
+//         "title": "Latest Quizzes",
+//         "description": "Take a break from the hard news of the day and enjoy a quiz on entertainment, sports, history and politics only from The Washington Times.",
+//         "url": "https://www.washingtontimes.com/quiz/",
+//         "urlToImage": null,
+//         "publishedAt": "2022-08-30T16:37:43.8583104Z",
+//         "content": "Featured Quizzes\r\nTake the challenge to learn about the life and career highlights of famed nonagenarian actress and comedian Betty White.\r\n Shares \r\nRead our synopsis and correctly identify a litera… [+32510 chars]"
+//       },
+//       {
+//         "source": { "id": "usa-today", "name": "USA Today" },
+//         "author": null,
+//         "title": "Daily Briefing",
+//         "description": "The day's top stories, from sports to movies to politics to world events.",
+//         "url": "https://profile.usatoday.com/newsletters/daily-briefing/",
+//         "urlToImage": "https://profile.usatoday.com/newsletters/resources/usat/property/usatoday/newsletter-thumbs/8872UT-E-NLETTER02@2x.jpg",
+//         "publishedAt": "2021-08-15T15:35:07+00:00",
+//         "content": "The day's top stories, from sports to movies to politics to world events."
+//       }
+//     ]
   
     constructor(){
         super();
         this.state={
-            articles:this.articles
+            // articles:this.articles,
+            articles:[],
+            loading:false
         }
+    }
+
+    async componentDidMount(){
+      console.log("cdm");
+      let url="https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=0046f61e768945c49b19651c3784cf82"
+      let data=await fetch(url);
+      let parsedData=await data.json();
+      console.log(parsedData);
+      this.setState({articles: parsedData.articles})
     }
 
   render() {
     return (
       <div className='container my-3'>
-        <h2>NewsMonkey - Top Headlines</h2>
+        <h1>NewsMonkey - Top Headlines</h1>
         <div className="row">
         {this.state.articles.map((element) => {
           return <div className="col-md-4" key={element.url}>
-          <NewsItem title={element.title.slice(0,40)} description={element.description.slice(0,88)} imageUrl={element.urlToImage} newsUrl={element.url}/>
+          <NewsItem title={element.title?element.title.slice(0,40):""} description={element.description?element.description.slice(0,88):""} imageUrl={element.urlToImage} newsUrl={element.url}/>
           </div>
         })}
 
--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -7,7 +7,7 @@ export default class NewsItem extends Component {
         
       <div className='my-3'>
         <div className="card" style={{width: "18rem"}}>
-            <img src={imageUrl} className="card-img-top" alt="..."/>
+            <img src={imageUrl?imageUrl:"https://cdn.benzinga.com/files/images/story/2022/11/09/disney-land-g9fd6f8df6_1920.jpg?width=1200&height=800&fit=crop"} className="card-img-top" alt="..."/>
             <div className="card-body">
                 <h5 className="card-title">{title}...</h5>
                 <p className="card-text">{description}...</p>
