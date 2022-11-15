c6f951c94b6b4034a07cfc0dff55fa137ec8d9d8
IshikaB3003 <ishikabansal3003@gmail.com>
2022-11-15 13:49:02 +0530

    added src tags,auth,date

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -2,15 +2,19 @@ import React, { Component } from 'react'
 
 export default class NewsItem extends Component {
   render() {
-    let {title,description,imageUrl,newsUrl}=this.props;
+    let {title,description,imageUrl,newsUrl,author,date,source}=this.props;
     return (
         
       <div className='my-3'>
         <div className="card">
+        <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left: '90%', zIndex:'1'}}>
+                  {source}
+                </span>
             <img src={imageUrl?imageUrl:"https://cdn.benzinga.com/files/images/story/2022/11/09/disney-land-g9fd6f8df6_1920.jpg?width=1200&height=800&fit=crop"} className="card-img-top" alt="..."/>
             <div className="card-body">
                 <h5 className="card-title">{title}...</h5>
                 <p className="card-text">{description}...</p>
+                <p className="card-text"><small className="text-muted">By {author?author:"unknown"} on {new Date(date).toGMTString()}</small></p>
                 <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-primary">Read More</a>
             </div>
             </div>
